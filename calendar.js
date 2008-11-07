491622109259f0942b22595fe06655ec99b55078
Dan Higham <danhigham@notux.(none)>
2008-11-07 23:26:15 +0000

    Added image for calendar background Fixed width

--- calendar.js
+++ calendar.js
@@ -18,12 +18,27 @@ function generateCalMarkup(month, year) {
 	var intEndDate = Date.parse(endYear + '/' + endMonth + '/01') - 86400000;
 	var monthEndDate = new Date(intEndDate) 
 
-    var startDate = intStartDate - (monthStartDate.getDay()*86400000);
-	var endDate = intEndDate + ((6 - monthEndDate.getDay()) * 86400000);
+  var startDate = intStartDate - ((monthStartDate.getDay() - 1)*86400000);
+	var endDate = intEndDate + ((7 - monthEndDate.getDay()) * 86400000);
  
-	alert(new Date(startDate));
-	alert(new Date(endDate));
-
-
+  var mainNode = document.createElement("div");
+  mainNode.id = "calendar";
+  
+  var calendarList = document.createElement("ul");
+  calendarList.className = "daylist";
+  
+  for(var x = startDate; x<=endDate; x+=86400000){
+    var dayNode = document.createElement("li")
+    var dayDate = new Date(x);
+    dayNode.innerHTML = dayDate.getDate();
+    
+    if ((dayDate.getMonth() + 1) != parseInt(month)) dayNode.className = "off-month";
+    
+    
+    calendarList.appendChild(dayNode);
+  }
+  
+  mainNode.appendChild(calendarList);
+  document.body.appendChild(mainNode);
 }
 
